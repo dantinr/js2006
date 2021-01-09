04fd70cb47916951debe4d9225716abbd800e8fa
dante <dantinr@gmail.com>
2021-01-09 14:51:19 +0800

    倒计时抽奖

--- 1-9/functions.js
+++ 1-9/functions.js
@@ -8,8 +8,8 @@ const week = [
     '星期六',
 ]
 
-
-function getDate2(seconds){
+//格式化倒计时
+function getDate2(seconds,type=1){
 
     //秒转天
     var d = seconds / 86400
@@ -18,17 +18,26 @@ function getDate2(seconds){
     //小时
     var h = seconds / 3600
     h = Math.floor(h) % 24
+    if(h<10){h='0'+h}
 
     //分钟
     var i = seconds / 60
     i = Math.floor(i) % 60
+    if(i<10){i='0'+i}
 
     //秒
     s = seconds % 60
+    if(s<10){s='0'+s}
 
-    return d + "天" + h + "小时" + i + "分钟" + s + "秒"
+    //时间显示格式
+    if(type==1){        // hh:mm:ss
+        return h + ":" + i + ":" + s
+    }else{
+        return d + "天" + h + "小时" + i + "分" + s + "秒"
+    }
 }
 
+//获取当前时间
 function getDate()
 {
     var t = new Date()

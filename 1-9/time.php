2e73504892514d17beee276315b1e7e58e76cd19
dante <dantinr@gmail.com>
2021-01-09 13:55:09 +0800

    倒计时

--- 1-9/time.php
+++ 1-9/time.php
@@ -1,4 +1,19 @@
 <?php
-  //  echo time();            //获取当前时间戳
-//echo '</br>';
-    echo date('Y-m-d H:i:s');
+    //放假时间  2021-1-30 18:00:00
+    $end =  strtotime("2021-1-30 18:00:00");
+    $seconds = $end - time();           //到结束时间所剩余的秒数
+
+
+    $response = [
+        'errno' => 0,
+        'msg'   => 'ok',
+        'data'  => [
+            'seconds'   => $seconds
+        ]
+    ];
+
+    echo json_encode($response);
+
+
+
+
